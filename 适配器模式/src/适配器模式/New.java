7520d8fedfe2adb76e4dfc756cefa235cf0e8a21
xuxin-123 <80077955+xuxin-123@users.noreply.github.com>
2021-06-11 16:14:45 +0800

    Delete 适配器模式 directory

--- 适配器模式/src/适配器模式/New.java
+++
@@ -1,7 +0,0 @@
-package ÊÊÅäÆ÷Ä£Ê½;
-
-public interface New {
-	public String newPort(MyFruit fruit1,MyFruit fruit2) {
-		
-	}
-}
--- 适配器模式/src/适配器模式/Old.java
+++
@@ -1,7 +0,0 @@
-package ÊÊÅäÆ÷Ä£Ê½;
-
-public class Old {
-public String oneport(MyFruit fruit) {
-	
-}
-}
