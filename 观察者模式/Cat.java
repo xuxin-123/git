e64a3789d7ea181f4e21859c3049b58583dc91e6
xuxin-123 <80077955+xuxin-123@users.noreply.github.com>
2021-06-19 18:08:14 +0800

    Add files via upload

---
+++ 观察者模式/Cat.java
@@ -0,0 +1,9 @@
+public class Cat extends MySubject{
+    public void cry(){
+        System.out.println("猫叫！");
+        System.out.println("----------------");
+        for(Object obs:observers){
+            ((MyObserver)obs).response();
+        }
+    }
+}
